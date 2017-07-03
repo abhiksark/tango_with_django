a1a0a36d2faf3dc34111fb4c7ea86470ba739694
abhiksark <abhiksark@gmail.com>
2017-07-03 14:55:42 +0530

    v2.3

--- tango_with_django_project/rango/admin.py
+++ tango_with_django_project/rango/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from rango.models import Category, Page	
+
 # Register your models here.
 admin.site.register(Category)
 admin.site.register(Page)
+
+class CategoryAdmin(admin.ModelAdmin):
+	prepopulated_fields = {'slug':('name',)}
--- tango_with_django_project/tango_with_django_project/urls.py
+++ tango_with_django_project/tango_with_django_project/urls.py
@@ -20,8 +20,17 @@ from django.conf import settings
 
 from django.conf.urls.static import static
 
-urlpatterns = [
+"""urlpatterns = [
 	url(r'^$',views.index,name='index'),
 	url(r'^rango/', include('rango.urls')),
     url(r'^admin/', admin.site.urls),
+]
+"""
+
+urlpatterns = [
+url(r'^$', views.index, name='index'),
+#url(r'^about/$', views.about, name='about'),
+url(r'^category/(?P<category_name_slug>[\w\-]+)/$',
+views.show_category, name='show_category'),
+url(r'^admin/', admin.site.urls),
 ]
